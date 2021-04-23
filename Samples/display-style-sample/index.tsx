1f1f04414ea646f5d591b3e903b645fb642bda02
Eimantas Daknys <Eimantas.Daknys@bentley.com>
2021-04-23 10:13:26 +0300

    hjlhkj

--- Samples/display-style-sample/index.tsx
+++ Samples/display-style-sample/index.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { getDisplayStylesSpec } from 'code/sampleSpec';
-import { DeveloperHarness } from '@bentley/itwinjs-sandbox-harness';
-import App from 'App';
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import React, { FunctionComponent, useState } from "react";
+import { AuthorizationClient, default3DSandboxUi, useSampleWidget, ViewSetup, SampleIModels } from "@itwinjs-sandbox";
+import { Viewer } from "@bentley/itwin-viewer-react";
+import { IModelConnection } from "@bentley/imodeljs-frontend";
+import { IModelViewportControlOptions } from "@bentley/ui-framework";
 
-// note: you can use App instead of DeveloperHarness here to run itwinjs viewer directly.
-//       It makes the project a regular react app, with debugging in vscode etc.
+const ViewportOnlyApp: FunctionComponent = () => {
+    const sampleIModelInfo = useSampleWidget("Use the toolbar at the top-right to navigate the model.");
+    const [viewportOptions, setViewportOptions] = useState<IModelViewportControlOptions>();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <DeveloperHarness spec={getDisplayStylesSpec()}></DeveloperHarness>
-    {/* <App></App> */}
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    const _oniModelReady = async (iModelConnection: IModelConnection) => {
+        const viewState = await ViewSetup.getDefaultView(iModelConnection);
+        setViewportOptions({ viewState });
+    };
+
+    return (
+        <>
+            { /* Viewport to display the iModel */}
+            {sampleIModelInfo?.contextId && sampleIModelInfo?.iModelId &&
+                <Viewer
+                    contextId={sampleIModelInfo.contextId}
+                    iModelId={sampleIModelInfo.iModelId}
+                    authConfig={{ oidcClient: AuthorizationClient.oidcClient }}
+                    viewportOptions={viewportOptions}
+                    defaultUiConfig={default3DSandboxUi}
+                    theme="dark"
+                    onIModelConnected={_oniModelReady}
+                />
+            }
+        </>
+    );
+};
+
+export default ViewportOnlyApp;
